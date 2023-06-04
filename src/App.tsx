f15f0e338f65f45d50a17bed71f01e523ba3f2e8
Ivan Cruz <ivancruzhn@gmail.com>
2023-06-03 18:02:04 -0600

    add ant desing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,78 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { country, CountryService, ApiError } from './services';
+import { Form, Input, InputNumber, Popconfirm, Table, Typography } from 'antd';
+/*
+interface Item {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
+const originData: Item[] = [];
+for (let i = 0; i < 100; i++) {
+  originData.push({
+    key: i.toString(),
+    name: `Edward ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`,
+  });
+}
+*/
+interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
+  editing: boolean;
+  dataIndex: string;
+  title: any;
+  inputType: 'number' | 'text';
+  record: country;
+  index: number;
+  children: React.ReactNode;
+}
+
+const EditableCell: React.FC<EditableCellProps> = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{ margin: 0 }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
+
 
 function App() {
 //Hook useState
 const [dataCountry,setDataCountry] = useState<country[]>([]);
 const [error, setError] = useState<ApiError>();
 //Hook useEffect (ejecutarlo cuando inicia por primera ves esta app)
+const [form] = Form.useForm();
+const [data, setData] = useState(dataCountry);
+const [editingKey, setEditingKey] = useState<String|undefined>('');
 
 useEffect(()=>{
   CountryService.getApiCountry()
@@ -14,16 +80,108 @@ useEffect(()=>{
     .catch((error)=>setError(error));
 },[])
 
+const isEditing = (record: country) => record.id === editingKey;
+
+  const edit = (record: country) => {
+    form.setFieldsValue({ name: '', ...record });
+    setEditingKey(record.id);
+  };
+
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const save = async (key: String | undefined) => {
+    try {
+      const row = (await form.validateFields()) as country;
+
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.id);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        setData(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
+  const columns = [
+    {
+      title: 'Country name',
+      dataIndex: 'country_name',
+      width: '25%',
+      editable: true,
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (_: any, record: country) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <Typography.Link onClick={() => save(record.id)} style={{ marginRight: 8 }}>
+              Save
+            </Typography.Link>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+            Edit
+          </Typography.Link>
+        );
+      },
+    },
+  ];
+
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record: country) => ({
+        record,
+        inputType: col.dataIndex === 'age' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
   return (
     <>
     <h1>Country</h1>
-    <ul>
-      {dataCountry.map((data)=>{
-        return(
-          <li key={data.id}>{data.country_name}</li>
-        )
-      })}
-    </ul>
+
+    <Form form={form} component={false}>
+      <Table
+        components={{
+          body: {
+            cell: EditableCell,
+          },
+        }}
+        bordered
+        size="large"
+        dataSource={dataCountry}
+        columns={mergedColumns}
+        rowClassName="editable-row"
+        rowKey="id"
+        pagination={{
+          onChange: cancel,
+        }}
+      />
+    </Form>
     </>
   )
 }
